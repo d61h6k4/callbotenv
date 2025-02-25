781e22728a28fe436f9eee8d652f403c910d9789
d61h6k4 <ddbihbka@gmail.com>
2025-02-25 21:01:02 +0100

    Refactor xvfb

--- examples/app/main.py
+++ examples/app/main.py
@@ -8,9 +8,9 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class Xvfb:
-    def __init__(
-        self, width: int = 960, height: int = 540, depth: int = 24, display: str = ":0"
-    ):
+    SLEEP_TIME_BEFORE_START = 0.1
+
+    def __init__(self, width: int = 960, height: int = 540, depth: int = 24, display: str = ":0"):
         self._cmd = [
             "Xvfb",
             display,
@@ -19,27 +19,30 @@ class Xvfb:
             f"{width}x{height}x{depth}",
             "-dpi",
             "96",
-            "-ac",
             "-f",
             "0",
             "-nocursor",
             "-nolisten",
             "tcp",
+            "-nolisten",
+            "unix",
             "+extension",
             "RANDR",
+            "+extension",
+            "GLX",
         ]
         self.proc = None
 
     def __enter__(self):
-        self.proc = subprocess.Popen(
-            self._cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
-        sout, serr = self.proc.communicate()
-        if serr:
-            _LOGGER.error(serr)
+        self.proc = subprocess.Popen(self._cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        if sout:
-            _LOGGER.info(sout)
+        _LOGGER.info(f"Xvfb started at {self.proc.pid} (returncode = {self.proc.returncode})")
+        # give Xvfb time to start
+        time.sleep(self.__class__.SLEEP_TIME_BEFORE_START)
+        ret_code = self.proc.poll()
+        if ret_code is not None:
+            _, err = self.proc.communicate()
+            raise RuntimeError(f"Xvfb did not start ({ret_code}): {shlex.join(self._cmd)}\n{err.decode('utf8')}")
 
         return self
 
@@ -66,15 +69,9 @@ class FFmpeg:
         self.proc = None
 
     def __enter__(self):
+        subprocess.run(shlex.split("ln -s /usr/lib/aarch64-linux-gnu/blas/libblas.so.3 /usr/lib/aarch64-linux-gnu/"))
         subprocess.run(
-            shlex.split(
-                "ln -s /usr/lib/aarch64-linux-gnu/blas/libblas.so.3 /usr/lib/aarch64-linux-gnu/"
-            )
-        )
-        subprocess.run(
-            shlex.split(
-                "ln -s /usr/lib/aarch64-linux-gnu/lapack/liblapack.so.3 /usr/lib/aarch64-linux-gnu/"
-            )
+            shlex.split("ln -s /usr/lib/aarch64-linux-gnu/lapack/liblapack.so.3 /usr/lib/aarch64-linux-gnu/")
         )
         self.proc = subprocess.Popen(self._cmd)
         return self
@@ -89,7 +86,7 @@ def main():
     display = os.getenv("DISPLAY", ":0")
     with Xvfb(display=display):
         with FFmpeg(display=display):
-            time.sleep(5.0)
+            time.sleep(1.0)
 
 
 if __name__ == "__main__":
